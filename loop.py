3d9deb1e9e771627a3c8006fafb98deff170d77c
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-06-02 22:22:18 +0530

    loop.py
    run this file with the python idle

---
+++ loop.py
@@ -0,0 +1,17 @@
+initial=0                #start with this integer value
+amount=10                #number of times the increment will occur
+time_difference=3        #time difference between two instance   
+
+
+from pynput.keyboard import Key, Controller
+import time
+
+keyboard = Controller()
+
+initial=initial-1
+
+for i in range(0, amount):
+    initial=initial+1
+    keyboard.type(str(initial))
+    keyboard.type("\n")
+    time.sleep(difference)
