689f73c141b7ba719add684a4dd035482cf249e6
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-06-02 22:52:43 +0530

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,6 +1,6 @@
 initial=0                #start with this integer value
-amount=10                #number of times the increment will occur
-time_difference=3        #time difference between two instance   
+amount=5                 #number of times the increment will occur
+time_difference=1        #time difference between two instance   
 
 
 from pynput.keyboard import Key, Controller
@@ -8,12 +8,12 @@ import time
 
 keyboard = Controller()
 
+print("Loop initialized from "+str(initial)+" to "+str(initial+amount)+".\n")
 initial=initial-1
-
-print("Loop initialized from"+initial+"to"+(initial+amount)+"\n")
+amount=amount+1
 for i in range(0, amount):
     initial=initial+1
     keyboard.type(str(initial))
     keyboard.type("\n")
     time.sleep(time_difference)
-print("Loop completed")
+print("Loop completed.")
